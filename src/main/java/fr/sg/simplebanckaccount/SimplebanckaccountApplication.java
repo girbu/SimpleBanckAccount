b3fae8c859d34131d0e81c927a9d79d6103aef45
igirbu <ion.girbu@capgemini.com>
2022-05-16 13:21:01 +0200

    Project Initiation

---
+++ src/main/java/fr/sg/simplebanckaccount/SimplebanckaccountApplication.java
@@ -0,0 +1,13 @@
+package fr.sg.simplebanckaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplebanckaccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplebanckaccountApplication.class, args);
+	}
+
+}
